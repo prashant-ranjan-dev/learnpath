171065530e971e508e6cd84bad6e294a98423e9f
Prashant Ranjan <prashantranjan201@gmail.com>
2025-12-24 19:18:59 +0530

    Add initial project structure with Maven configuration and Spring Boot setup
    - Initialize pom.xml with Spring Boot dependencies and configurations using Spring Boot initializer

---
+++ learnpath-backend/src/test/java/com/learnpath/learnpath_backend/LearnpathBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnpath.learnpath_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnpathBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
