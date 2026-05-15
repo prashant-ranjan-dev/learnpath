f2059b02d724567b444daef7c5dd6fb2e671742e
Prashant Ranjan <prashantranjan201@gmail.com>
2026-05-15 22:35:32 +0530

    Updated the Module class name to SyllabusModule to avoid confusion between the java.lang.Module package. And implemented all the necessary fields in SyllabusModule class with necessary annotations.

---
+++ src/main/java/com/learnpath/version1/entities/SyllabusModule.java
@@ -0,0 +1,33 @@
+package com.learnpath.version1.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "syllabusModule")
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class SyllabusModule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String title;
+
+    private double estimatedHours;
+
+    @ManyToOne
+    @JoinColumn(name ="syllabus_id")
+    private Syllabus syllabus;
+
+    @OneToMany(mappedBy = "syllabusModule")
+    private List<Topic> topics;
+}
