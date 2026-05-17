a0c6e32317fad754bbd9b45b509fce6c3d66096d
Prashant Ranjan <prashantranjan201@gmail.com>
2026-05-17 12:03:25 +0530

    New Exception to handle Ai Response Parsing

---
+++ src/main/java/com/learnpath/version1/exception/AiResponseParsingException.java
@@ -0,0 +1,7 @@
+package com.learnpath.version1.exception;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class AiResponseParsingException extends RuntimeException{
+}
