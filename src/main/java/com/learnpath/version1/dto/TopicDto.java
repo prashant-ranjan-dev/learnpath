4259dcda60ff910c02d1fd708fe15e20d6fc04bf
Prashant Ranjan <prashantranjan201@gmail.com>
2026-05-21 11:11:57 +0530

    Updated the Topic and TopicDto to remove the errors and match the variable names.

--- src/main/java/com/learnpath/version1/dto/TopicDto.java
+++ src/main/java/com/learnpath/version1/dto/TopicDto.java
@@ -1,7 +1,11 @@
 package com.learnpath.version1.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public record TopicDto(
         Long id,
-        String names
+        String name
 ) {
 }
