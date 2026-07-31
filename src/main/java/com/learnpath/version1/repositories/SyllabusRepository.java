d47ec563d2489069edf81bcc838b84b3437dd42e
Prashant Ranjan <prashantranjan201@gmail.com>
2026-07-31 18:33:29 +0530

    Introduced SyllabusRepository.findByUserOrderByCreatedAtDesc method to access the syllabi in desc order based on the local time data type introduced in the syllabus entity.

--- src/main/java/com/learnpath/version1/repositories/SyllabusRepository.java
+++ src/main/java/com/learnpath/version1/repositories/SyllabusRepository.java
@@ -1,8 +1,13 @@
 package com.learnpath.version1.repositories;
 
 import com.learnpath.version1.entities.Syllabus;
+import com.learnpath.version1.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SyllabusRepository extends JpaRepository<Syllabus, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface SyllabusRepository extends JpaRepository<Syllabus, Long> {
+    List<Syllabus> findByUserOrderByCreatedAtDesc(User user);
+    Optional<Syllabus> findByIdAndUser(Long id, User user);  // not strictly needed
 }
