21db99dd9601f2eb3c4b7dbee7b527649e885d57
Prashant Ranjan <prashantranjan201@gmail.com>
2026-07-31 18:34:25 +0530

    added the necessary imports in the SyllabusResponse.java file

--- src/main/java/com/learnpath/version1/dto/SyllabusResponse.java
+++ src/main/java/com/learnpath/version1/dto/SyllabusResponse.java
@@ -1,8 +1,7 @@
 package com.learnpath.version1.dto;
 
+import com.learnpath.version1.dto.ModuleDto;
+
 import java.util.List;
 
-public record SyllabusResponse(
-        Long syllabusId,
-        List<ModuleDto> modules) {
-}
+public record SyllabusResponse(Long syllabusId, String topic, List<ModuleDto> modules) {}
